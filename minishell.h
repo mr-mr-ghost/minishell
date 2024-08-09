1a1a1061ed71bb7cdef35f39f9ceb7a4fdcb8675
jhoddy <jack.a.hoddy@gmail.com>
2024-08-09 16:21:53 +0200

    Added header files

--- minishell.h
+++ minishell.h
@@ -10,12 +10,31 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+TODO: Allow input
+TODO: Split input into tokens
+TODO: Write Commands
+TODO: Piping/flags/etc...
+TODO: Parsing
+TODO: Fix misc. errors
+*/
+
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
 # include <stdio.h>
+# include <string.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <sys/ioctl.h>
+# include <signal.h>
+# include <dirent.h>
+# include <termios.h>
+# include <curses.h>
+# include <term.h>
 # include "libft/libft.h"
 
 #endif
