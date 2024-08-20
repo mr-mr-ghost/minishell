65ab5517727d31709e39632629cf516864e07c67
mr-mr-ghost <jack.a.hoddy@gmail.com>
2024-08-20 21:59:17 +0200

    updated cd

--- builtin_cd.c
+++ builtin_cd.c
@@ -31,6 +31,23 @@ char	*set_back_dir(t_env *env)
 	return (tmp);
 }
 
+int	replace_path(t_env *env, char *name, char *path)
+{
+	char	*line;
+	char	*tmp;
+
+	line = find_env_line(env, name);
+	if (!line)
+		return (1);
+	tmp = ft_strjoin(name, "=");
+	free(line);
+	line = ft_strjoin(tmp, path);
+	delete_replace(&env, name, line);
+	free(tmp);
+	free(line);
+	return (0);
+}
+
 int	cd_command(t_data *data, t_token *token)
 {
 	char	*path;
@@ -42,11 +59,7 @@ int	cd_command(t_data *data, t_token *token)
 		return (1);
 	cd_token = token->next;
 	if (!cd_token)
-	{
 		path = find_env_value(data->env, "HOME");
-		if (!path)
-			return (1);
-	}
 	else if (!ft_strcmp(cd_token->value, "-"))
 		path = find_env_value(data->env, "OLDPWD");
 	else if (!ft_strcmp(cd_token->value, "."))
@@ -55,12 +68,14 @@ int	cd_command(t_data *data, t_token *token)
 		path = set_back_dir(data->env);
 	else
 		path = cd_token->value;
+	if (!path)
+		return (1);
 	if (chdir(path) == -1)
 	{
 		printf("cd: %s: %s\n", path, strerror(errno));
 		return (1);
 	}
-	delete_replace(&data->env, "OLDPWD", old_pwd);
-	delete_replace(&data->env, "PWD", path);
+	replace_path(data->env, "OLDPWD", old_pwd);
+	replace_path(data->env, "PWD", getcwd(NULL, 0));
 	return (0);
 }
